4903d327af9b4f2539099ea3605a459e2726d1bd
Sergio Valero García <svalero@nextdigital.es>
2019-10-31 19:19:18 +0100

    switchAnimal

--- src/game/scenes/PlayScene.js
+++ src/game/scenes/PlayScene.js
@@ -26,13 +26,18 @@ export default class PlayScene extends Scene {
           ];
         
           this.animals = this.add.group();
+          this.animals.customParams = {actual: 0};
+          var self = this;   
 
-          var self = this;    
-          var animal;
           animalData.forEach(function(element){
             //create each animal and save it's properties
-            animal = self.animals.create(centerX,centerY, element.key);
+            var animal = self.animals.create(centerX,centerY, element.key, false, false);
+            animal.setInteractive(self.input.makePixelPerfect())
+                .on('pointerdown',self.animateAnimal);
           });
+
+        //this.animalIterator = this.animals.next();
+        this.currentAnimal = this.animals.getFirst(true).setVisible(true);
 
         this.rightArrow = this.add.sprite(centerX + (centerX * 0.8), centerY, 'arrow');
         this.rightArrow.customParams = { direction: 1 };
@@ -42,7 +47,7 @@ export default class PlayScene extends Scene {
 
 
         this.leftArrow = this.add.sprite(centerX - (centerX * 0.8), centerY, 'arrow');
-        this.leftArrow.customParams = { direction: -1 };
+        this.leftArrow.customParams = { direction: -1, active: false };
         this.leftArrow.flipX = -1;
 
         this.leftArrow.setInteractive(this.input.makePixelPerfect()).on('pointerdown',
@@ -52,11 +57,46 @@ export default class PlayScene extends Scene {
 
     update() {
 
+        if (this.leftArrow.customParams.active) {
+            this.getPrevious(this)
+        }
 
+        if (this.rightArrow.customParams.active) {
+            this.getNext(this)
+        }
 
     }
     switchAnimal() {
-        console.log('move animal')
+        this.customParams.active = true;
     }
+
+    getNext(context){
+        context.currentAnimal.setVisible(false);
+        if(context.animals.customParams.actual < context.animals.getChildren().length -1){
+            context.animals.customParams.actual += 1; 
+        } else {
+            context.animals.customParams.actual = 0; 
+        }
+        context.currentAnimal = this.animals.getChildren()[context.animals.customParams.actual];
+        context.currentAnimal.setVisible(true);
+        context.rightArrow.customParams.active = false;
+    }
+
+    getPrevious(context){
+        context.currentAnimal.setVisible(false);
+        if(context.animals.customParams.actual > 0){
+            context.animals.customParams.actual -= 1; 
+        } else {
+            context.animals.customParams.actual = this.animals.getChildren().length - 1; 
+        }
+        context.currentAnimal = this.animals.getChildren()[context.animals.customParams.actual];
+        context.currentAnimal.setVisible(true);
+        context.leftArrow.customParams.active = false;
+
+    }
+
+    animateAnimal () {
+        console.log('animate animal');
+      }
 
 }
